1d8a92f3c859637bf2a5ebcdce235c7cfb8c70ee
MatthieuCadeau <44285767+MatthieuCadeau@users.noreply.github.com>
2018-10-19 10:18:46 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
+<html>
+<body>
 <?php
 echo("Moi j'aime la Leffe et le whisky");
 $date = date("19/09/2018");
 $heure = date("10:13:55");
 echo("Nous sommes le $date et il est actuellement $heure");
 ?>
+</body>
+</html>
