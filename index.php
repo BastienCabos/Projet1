092db2fceccdc02e660cdf7cb27a4d1079dadea6
MatthieuCadeau <44285767+MatthieuCadeau@users.noreply.github.com>
2018-10-19 11:55:06 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 <body>
 <?php
-echo("Moi j'aime la Leffe et le whisky");
+echo("Nous essayons de faire une page PHP");
 $date = date("d/m/Y");
 $heure = date("H:i:s");
 echo("Nous sommes le $date et il est actuellement $heure");
